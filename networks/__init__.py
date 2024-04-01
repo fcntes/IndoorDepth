8c7cda8e85bcd193547433477c62b2d09aa33c42
fcntes <fcntes@users.noreply.github.com>
2024-04-01 21:50:17 +0800

    Delete networks/__init__.py

--- networks/__init__.py
+++
@@ -1,9 +0,0 @@
-from .resnet_encoder import ResnetEncoder
-from .depth_decoder import DepthDecoder
-
-from .pose_decoder import PoseDecoder
-from .pose_cnn import PoseCNN
-####add
-from .pose_decoder_rec import PoseDecoderRec
-from .pose_decoder_third import PoseDecoderThird
-from .pose_decoder_fourth import PoseDecoderFourth
