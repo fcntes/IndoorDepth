8e405f8eca2b0dc67a7ad1e0caae72bcf71322cb
fcntes <fcntes@users.noreply.github.com>
2024-04-01 21:51:07 +0800

    Add files via upload

---
+++ networks/__init__.py
@@ -0,0 +1,8 @@
+from .resnet_encoder import ResnetEncoder
+from .depth_decoder import DepthDecoder
+
+from .pose_decoder import PoseDecoder
+from .pose_cnn import PoseCNN
+####add
+from .pose_decoder_rec import PoseDecoderRec
+from .pose_decoder_third import PoseDecoderThird
